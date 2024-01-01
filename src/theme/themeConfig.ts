62b78caacf85a6abe0bdfd9f97827649979e0978
AnupamRawal <rawalanupam5@gmail.com>
2024-01-01 20:18:50 +0530

    antd design added, application configured

---
+++ src/theme/themeConfig.ts
@@ -0,0 +1,11 @@
+// theme/themeConfig.ts
+import type { ThemeConfig } from 'antd';
+
+const theme: ThemeConfig = {
+  token: {
+    fontSize: 16,
+    colorBorderBg:'#E1E3E5'
+  },
+};
+
+export default theme;
